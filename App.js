0afadbafa8abc3b0bdc24358a067c1f316c7ed4c
minari515 <126942302+minari515@users.noreply.github.com>
2023-03-04 16:03:11 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MovieUploader from "./MovieUploader";
+
+function App() {
+  return (
+    <div className="App">
+      {/* uplodaer */}
+      <MovieUploader />
+    </div>
+  );
+}
+
+export default App;
